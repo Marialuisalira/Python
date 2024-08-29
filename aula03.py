665486efc5052b428115eaba2a87a51088621f24
Maria Luísa <mmarialuisa3001@gmail.com>
2024-08-29 11:50:56 -0300

    Add files via upload

---
+++ aula03.py
@@ -0,0 +1,18 @@
+
+# Variáveis em Python
+# Não há necessidade de declrá-las no início de progamas
+# Basta criar no meio do algoritmo com atribuições 
+
+teste = "Maria Luisa"
+print(teste) # Imprime  a string "Maria Luisa"
+print(type(teste)) # Imprime o tipo de varíavel, string
+teste = 10 
+print(teste) # Imprime o número 10
+print(type(teste)) # Imprime o tipo de varíavel, que int
+teste = 1.74
+print(teste) # Imprime o número 1.74
+print(type(teste)) # Imprime o tipo de varíavel, que é float
+teste = True 
+print(teste) # Imprime o valor True
+print(type(teste)) # Imprime o tipo da varíavel, que é bool
+
